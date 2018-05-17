806ef24784ee30353bc9ff006e5a25da7e4a3e93
sopa <faton.sopa@manaferra.com>
2018-05-17 13:49:44 +0200

    default theme deploy

---
+++ views/pages/home.blade.php
@@ -0,0 +1,21 @@
+@extends('DefaultTheme.views.index')
+
+@section('meta')
+    @parent
+    {{metaTags(homepage())}}
+@endsection
+
+@section('header')
+    @parent
+@endsection
+
+@section('content')
+    <div class="container">
+        <h1>{{homepage()->title}}</h1>
+        <p class="lead">
+            <?php
+                print homepage()->content();
+            ?>
+        </p>
+    </div>
+@endsection
